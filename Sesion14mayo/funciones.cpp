4c81fa3339e8547a95ed101b9806aedf20f9ecc9
AliciaEA <aliestrada@uamv.edu.ni>
2024-05-14 14:51:18 -0600

    FUnciones agregar propias

--- Sesion14mayo/funciones.cpp
+++ Sesion14mayo/funciones.cpp
@@ -4,9 +4,15 @@
 
 /*Saludar*/
 void showMessage();
+int sumatoria(int num1, int num2);
 
 void showMessage()
 {
     printf("Universidad Americana \n");
     printf("UAM \n");
+}
+
+int sumatoria(int num1, int num2)
+{
+    return num1 + num2;
 }
--- Sesion14mayo/principal.cpp
+++ Sesion14mayo/principal.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int num1, num2;
     showMessage();
+    cout << "Proporcione el primer valor: ";
+    cin >> num1;
+    cout << "Proporcione el segundo valor: ";
+    cin >> num2;
+
+    cout << "La suma de ambos numeros es: " << sumatoria(num1, num2);
     return 0;
 }
